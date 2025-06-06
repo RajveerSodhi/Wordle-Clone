388931a53ce854b0e2559980880f4bb9e95a842d
RajveerSodhi <rajveersodhi03@gmail.com>
2025-06-05 23:42:06 -0700

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,8 +5,8 @@ function Footer() {
         <footer>
             <ul>
                 <li>Wordle Clone by <a href="https://www.rajveersodhi.com" target = "_blank" rel="noreferrer">Rajveer Sodhi</a></li>
-                <li>|</li>
-                <li>Dedicated to my Dad for getting me hooked!</li>
+                {/* <li>|</li>
+                <li>Dedicated to my Dad for getting me hooked!</li> */}
                 <li>|</li>
                 <li><a href="https://www.nytimes.com/games/wordle/index.html" target = "_blank" rel="noreferrer">Visit Wordle</a></li>
             </ul>
