96fd6ca6b15c0cc93668b55b1d035cdad845994b
RajveerSodhi <rajveersodhi03@gmail.com>
2025-09-06 02:39:20 -0400

    added custom game examples

--- src/Components/CreateGameDialog.jsx
+++ src/Components/CreateGameDialog.jsx
@@ -8,7 +8,7 @@ import { useState } from "react";
 function CreateGameDialog({onClose, setToastType, showLobbyScreen}) {
     const [customChars, setCustomChars] = useState(0);
     const [inputValue, setInputValue] = useState('');
-    const gameExamples = ["HBDAY", "BFF", "SCHOOL", "POTTER", "HOGWARTS", "WELCOME", "SWITCH", "RAJVEER", "PRANK", "FISH", "ASH", "SQUIRTLE", "TRUMP", "JAPAN", "ILOVEYOU"];
+    const gameExamples = ["HBDAY", "BFF", "SCHOOL", "POTTER", "HOGWARTS", "WELCOME", "SWITCH", "RAJVEER", "PRANK", "FISH", "FIFA", "JUHI", "ARSH", "ASH", "SQUIRTLE", "TRUMP", "JAPAN", "ILOVEYOU", "GANESHA", "PREMJEET", "SIMRAN"];
 
     const handleInputChange = (event) => {
         const value = event.target.value;
